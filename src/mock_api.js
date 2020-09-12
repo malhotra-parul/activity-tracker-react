b692178c92a3bab3afa008264b5a876933a7d2b4
malhotra-parul <parulm.business@gmail.com>
2020-09-12 11:04:56 +0530

    updated Readme file

--- src/mock_api.js
+++ src/mock_api.js
@@ -30,15 +30,15 @@ export const data = {
 			"photo": photoTwo,
 			"activity_periods": [{
 					"start_time": "Feb 1 2020  1:33PM",
-					"end_time": "Feb 1 2020 1:54PM"
+					"end_time": "Feb 1 2020 2:54PM"
 				},
 				{
-					"start_time": "Mar 1 2020  11:11AM",
-					"end_time": "Mar 1 2020 2:00PM"
+					"start_time": "Mar 1 2020  12:11AM",
+					"end_time": "Mar 1 2020 3:00PM"
 				},
 				{
-					"start_time": "Mar 16 2020  5:33PM",
-					"end_time": "Mar 16 2020 8:02PM"
+					"start_time": "Mar 16 2020  6:33PM",
+					"end_time": "Mar 16 2020 9:02PM"
 				}
 			]
 		}
